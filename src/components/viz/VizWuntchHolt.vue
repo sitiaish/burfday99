a27351d0db7637f8da8c53ad1fae4ac4e803818b
SK <limsweekiat@gmail.com>
2021-07-18 22:43:05 +0800

    Delete unnecessary code

--- src/components/viz/VizWuntchHolt.vue
+++ src/components/viz/VizWuntchHolt.vue
@@ -21,52 +21,11 @@
 
 <script>
 import * as d3 from 'd3';
-import _kebabCase from 'lodash.kebabcase';
 
 export default {
   data() {
     return {
-      tooltip: {
-        show: false,
-        name: '',
-        hex: '',
-        move: {
-          top: 0,
-          left: 0,
-        },
-      },
-      data: [
-        {
-          "who": "wuntch",
-          "line": "Hello, Raymond.",
-          "length": 2,
-          "type": "no"
-        },
-        {
-          "who": "holt",
-          "line": "Captain Wuntch. Good to see you.",
-          "length": 6,
-          "type": "no"
-        },
-        {
-          "who": "holt",
-          "line": "But if you're here, who's guarding Hades?",
-          "length": 7,
-          "type": "insult"
-        },
-        {
-          "who": "wuntch",
-          "line": "It's deputy chief Wuntch now. ",
-          "length": 5,
-          "type": "no"
-        },
-        {
-          "who": "wuntch",
-          "line": "Unsurprisingly, I've been promoted above you. And now I'm here to evaluate you.",
-          "length": 13,
-          "type": "insult"
-        }
-      ]
+      data: []
     };
   },   
   mounted() {
@@ -81,70 +40,7 @@ export default {
   methods: {
     async loadData() {
       this.data = await d3.csv('/data/wuntch-holt.csv')
-      this.$nextTick(() => {
-        console.log('test')
-        console.log(this.groupedData)
-      })
-    },
-    formatData() {
-      // load csv
-      // group data by id
-      // for each id to create a div in v-col cols="3"
-      // in the div, append paragraphs based on the lines
-      // style the lines based on who said it and if it is an insult
-    },       
-    handleTooltip() {
-      this.rect
-        .on('mouseenter.data', (event, d) => {
-          this.tooltip.show = true;
-          this.tooltip.name = d.title;
-          this.tooltip.hex = d.hex;
-        })
-        .on('mouseenter.design', function showBox() {
-          // highlight the circle
-          d3.select(this).attr('stroke', '#2c3e50').raise();
-          // move the tooltip to the correct position
-          const selectedRect = d3
-            .select(this)
-            .node()
-            .getBoundingClientRect();
-          const tooltip = d3
-            .select('.viz-tooltip')
-            .node()
-            .getBoundingClientRect();
-          const chart = d3
-            .select('#chartWuntchHolt')
-            .node()
-            .getBoundingClientRect();
-          
-          const posLeft =
-            selectedRect.left > document.body.clientWidth / 2.25
-              ? selectedRect.left - chart.left - tooltip.width - 5// right
-              : selectedRect.right - chart.left + 5  // left
-
-          const posTop = 
-            selectedRect.top > document.body.clientHeight / 2.5
-              ? selectedRect.bottom - chart.top - tooltip.height // bottom
-              : selectedRect.top - chart.top // top
-          
-          d3.select('.viz-tooltip')
-            .style('top', `${posTop}px`)
-            .style('left', `${posLeft}px`);
-        })
-        .on('mouseleave.design', function hideBox() {
-          // remove highlight from the circle
-          d3.select(this).attr('stroke', d => d.hex === '#FFFFFF' ? '#ededed' : 'none');
-        })
-        .on('mouseleave.data', () => {
-          // reset tooltip settings
-          this.tooltip.show = false;
-          this.tooltip.name = '';
-          this.tooltip.hex = ''; 
-        });
-    },
-    kebabCase(string) {
-      return _kebabCase(string)
-    },       
+    }
   },
 };
 </script>
