6b9239ac995cc6216410eaa69a3ebe0078b9f24d
SK <limsweekiat@gmail.com>
2021-07-20 19:48:07 +0800

    Update wuntch-holt section

--- src/components/viz/VizWuntchHolt.vue
+++ src/components/viz/VizWuntchHolt.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="pa-12">
-    <v-row class="data-row" no-gutters align="center" justify="center" v-for="(data, id) in groupedData" :key="id">
-      <!-- use a v-for to populate the divs  -->
-      <v-col cols="1">
-        <h2>S{{data[1][0].season}}E{{data[1][0].episode}}</h2>
-      </v-col>
-      <v-col cols="8">
-        <div class="pa-3">
-          <p 
-            v-for="(n, i) in data[1]"
-            :key="i"
-            :class="['mb-0, line', n.who === 'Wuntch' || n.who === 'Holt' ? n.who : 'other', n.type]"> 
-              {{n.line}}
-          </p>
-        </div>
-      </v-col>
-    </v-row>
+    <v-select 
+        style="width: 130px;"
+        item-text="name"
+        item-value="value"
+        v-model="insultType"      
+        :items="insultTypes"
+        @input="updateChart()" />
+    <v-container>
+      <h3>Insults Given</h3>
+      <svg id='wuntch-holt' style="border: 1px solid black;"/>
+    </v-container>
+    <v-container style="height: 250px; overflow: scroll;">
+      <v-row class="data-row" no-gutters align="center" justify="center" v-for="(data, id) in groupedData" :key="id">
+        <!-- use a v-for to populate the divs  -->
+        <v-col cols="1">
+          <h2>S{{data[1][0].season}}E{{data[1][0].episode}}</h2>
+        </v-col>
+        <v-col cols="8">
+          <div class="pa-3">
+            <p 
+              v-for="(n, i) in data[1]"
+              :key="i"
+              :class="['mb-0, line', n.who === 'Wuntch' || n.who === 'Holt' ? n.who : 'other', n.type === '' ? '' : 'insult']"> 
+                {{n.line}}
+            </p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -25,7 +38,15 @@ import * as d3 from 'd3';
 export default {
   data() {
     return {
-      data: []
+      data: [],
+      insultType: 'all',
+      insultTypes: [
+        {name: "Fantasy", value: 'fantasy'},
+        {name: "Animal", value: 'animal'},
+        {name: "Physical", value: 'physical'},
+        {name: "Others", value: 'others'},
+        {name: "All", value: 'all'}
+      ],
     };
   },   
   mounted() {
@@ -34,12 +55,44 @@ export default {
   },
   computed: {
     groupedData() {
-      return d3.group(this.data, d => d.id)
+      return d3.filter(d3.groups(this.data, d => d.id), d => d[1].some(e => e.type === this.insultType || this.insultType === 'all'))
     }
   },
   methods: {
     async loadData() {
-      this.data = await d3.csv('/data/wuntch-holt.csv')
+      this.data = await d3.csv('/data/wuntch-holt-new.csv')
+      this.$nextTick(() => {
+        this.updateChart()
+      })
+    },
+    updateChart() {
+      const svg = d3.select('svg#wuntch-holt')
+                    .attr('height', 350)
+                    .attr('width', 1200)
+
+      const data = d3.groups(d3.filter(this.data, d => d.type === this.insultType || this.insultType === 'all'), d => d.who)
+
+      let bars = svg.selectAll('rect.insult-bars')
+        .data(data)
+      bars.exit().remove()
+      bars.enter()
+        .append('rect')
+        .attr('class', 'insult-bars')
+        .merge(bars)
+        .attr('x', 10)
+        .attr('y', (d, i) => i * 30)
+        .attr('fill', d => {
+          switch(d[0]) {
+            case 'Holt':
+              return 'blue'
+            case 'Wuntch':
+              return 'red'
+            default:
+              return 'grey'
+          }
+        })
+        .attr('width', d => d[1].length * 5)
+        .attr('height', 20)
     }
   },
 };
