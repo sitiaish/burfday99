e37e9d488caf6adcbb4ab76f204a964f9799a97d
SK <limsweekiat@gmail.com>
2021-07-22 23:16:56 +0800

    Update wuntch-holt graph

--- src/components/viz/VizWuntchHolt.vue
+++ src/components/viz/VizWuntchHolt.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="pa-12">
-    <v-select 
+  <div class="pa-12" style="background: white;">
+    <v-container>
+      <p class="text--h2 font-tertiary text-center">
+        A quick look at insults across the seasons
+      </p>
+      <svg id='wuntch-holt'/>
+    </v-container>
+    <!-- <v-select 
         style="width: 130px;"
         item-text="name"
         item-value="value"
@@ -9,21 +15,32 @@
         @input="updateChart()" />
     <v-container>
       <h3>Insults Given</h3>
-      <svg id='wuntch-holt' style="border: 1px solid black;"/>
-    </v-container>
-    <v-container style="height: 250px; overflow: scroll;">
-      <v-row class="data-row" no-gutters align="center" justify="center" v-for="(data, id) in groupedData" :key="id">
-        <!-- use a v-for to populate the divs  -->
-        <v-col cols="1">
-          <h2>S{{data[1][0].season}}E{{data[1][0].episode}}</h2>
+      <svg id='wuntch-holt2' style="border: 1px solid black;"/>
+    </v-container> -->
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="2">
+          <img :src="charImage" style="width: 100px; margin: auto; display: block;"/>
         </v-col>
+        <v-col cols="3" v-if="line">
+          <h3 style="text-align: center;">"{{ line }}"</h3>
+        </v-col>
+      </v-row>
+      <v-row v-if="foundIn">
+        <h4 style="background: #fff534;">{{ foundIn }}</h4>
+      </v-row>
+      <v-row class="data-row" no-gutters align="center" justify="center" v-for="(data, id) in selectedConvo" :key="id">
+        <!-- use a v-for to populate the divs  -->
+        <!-- <v-col cols="1">
+          <h2>S{{data[1][0].season}}E{{data[1][0].episode}}</h2>
+        </v-col> -->
         <v-col cols="8">
           <div class="pa-3">
             <p 
               v-for="(n, i) in data[1]"
               :key="i"
-              :class="['mb-0, line', n.who === 'Wuntch' || n.who === 'Holt' ? n.who : 'other', n.type === '' ? '' : 'insult']"> 
-                {{n.line}}
+              :class="['mb-0, line', n.who === 'Wuntch' || n.who === 'Holt' ? n.who : 'other', n.type === '' ? '' : 'insult', n.line === line ? 'selected-insult' : '']"> 
+                {{ n.line }}
             </p>
           </div>
         </v-col>
@@ -47,6 +64,10 @@ export default {
         {name: "Others", value: 'others'},
         {name: "All", value: 'all'}
       ],
+      line: '',
+      selectedConvoId: -1,
+      charImage: '',
+      foundIn: ''
     };
   },   
   mounted() {
@@ -56,17 +77,95 @@ export default {
   computed: {
     groupedData() {
       return d3.filter(d3.groups(this.data, d => d.id), d => d[1].some(e => e.type === this.insultType || this.insultType === 'all'))
+    },
+    selectedConvo() {
+      return d3.filter(d3.groups(this.data, d => d.id), d => d[0] === this.selectedConvoId)
     }
   },
   methods: {
     async loadData() {
       this.data = await d3.csv('/data/wuntch-holt-new.csv')
       this.$nextTick(() => {
-        this.updateChart()
+        // this.updateChart()
+        this.updateChartNew()
       })
     },
-    updateChart() {
+    updateChartNew() {
       const svg = d3.select('svg#wuntch-holt')
+                    .attr('height', 300)
+                    .attr('width', 1200)
+      const data = d3.groups(d3.filter(this.data, d => d.type != '' && (d.who === 'Holt' || d.who === 'Wuntch')), d => d.type, d => d.who)
+      console.log(data)
+      let insultTypes = svg.selectAll('rect.insult-bars')
+        .data(data).enter()
+        .append('g')
+        .attr('class', 'insult-types')
+        .attr('transform', d => {
+          const insultTypesY = {
+            fantasy: 0,
+            work: 1,
+            physical: 2,
+            animal: 3,
+            others: 4,
+          }
+          return 'translate(600 ' + (insultTypesY[d[0]] * 50 + 25) + ')'
+        })
+
+      insultTypes.append('text')
+        .text(d => d[0])
+        .attr('text-anchor', 'middle')
+        .attr('y', 15)
+        // .style('font-family', 'Anton')
+        .style('font-weight', 'bold')
+        .style('font-size', 32)
+
+      let insultWhos = insultTypes.selectAll('g')
+        .data(d => d[1]).enter()
+        .append('g')
+      
+      insultWhos.selectAll('rect')
+        .data(d => d[1]).enter()
+        .append('rect')
+        .attr('who', d => d.who)
+        .attr('x', (d, i) => d.who === 'Wuntch' ? 28 * (i + 2) - 12 : -28 * (i + 2) - 12)
+        .attr('y', -5)
+        .attr('height', 30)
+        .attr('width', 28)
+        .attr('fill', d => {
+          switch (d.who) {
+            case 'Holt': return '#1e3799'
+            case 'Wuntch': return '#eb2f06'
+            default: return 'grey'
+          }
+        })
+        .on("mouseover", (target, d) => {
+          this.selectedConvoId = d.id
+          this.line = d.line
+          this.charImage = d.who === 'Holt' ? 'holt_dp.png' : 'wuntch_dp.png'
+          this.foundIn = `Found in S${d.season}E${d.episode}...`
+          d3.select(target.target)
+            .style('cursor', 'pointer')
+            .attr('stroke', '#fff534')
+            .attr('fill', '#fff534')
+        })
+        .on('mouseout', target => {
+          d3.select(target.target)
+            .attr('stroke', 'none')
+            .attr('fill', d => d.who === 'Holt'? '#1e3799' : '#eb2f06')
+        })
+
+      insultWhos.selectAll('text')
+        .data(d => d[1]).enter()
+        .append('text')
+        .attr('who', d => d.who)
+        .attr('x', (d, i) => d.who === 'Wuntch' ? 28 * (i + 2) - 12 : -28 * (i + 2) - 12)
+        .attr('y', 20)
+        .text('💀')
+        .attr('font-size', 28)
+        .style('pointer-events', 'none')
+    },
+    updateChart() {
+      const svg = d3.select('svg#wuntch-holt2')
                     .attr('height', 350)
                     .attr('width', 1200)
 
@@ -137,12 +236,12 @@ export default {
 }
 
 .data-row {
-  background-color: rgba(0, 0, 0, 0.1);
+  background-color: rgba(255, 255, 255, 0.7);
   margin: 2px;
 }
 
 .data-row:hover {
-  background-color: rgba(255, 255, 255, 0.7);
+  background-color: rgba(0, 0, 0, 0.1);
   cursor: pointer;
 }
 
@@ -167,5 +266,13 @@ export default {
 .insult {
   background-image: unset !important;
   color: white;
+}
+
+.selected-insult {
+  border: 3px solid yellow;
+}
+
+text {
+  cursor: pointer;
 }
 </style>
